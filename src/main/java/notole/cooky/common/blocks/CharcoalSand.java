0af52ebd632468d7dc55a6e45b4beed4c531b9f2
tolikalokogolik <natali.atamanova1@gmail.com>
2022-12-08 12:46:37 +0200

    Initial commit

---
+++ src/main/java/notole/cooky/common/blocks/CharcoalSand.java
@@ -0,0 +1,18 @@
+package notole.cooky.common.blocks;
+
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import org.openjdk.nashorn.internal.ir.Block;
+import org.openjdk.nashorn.internal.ir.Statement;
+
+import java.util.List;
+import java.util.Properties;
+
+public class CharcoalSand extends Block {
+
+
+    public CharcoalSand(long token, int finish, int flags, Statement... statements) {
+        super(token, finish, flags, statements);
+    }
+
+
+}
